c9278ebadeacc3cbb0223e686445acaad104b4df
howardmann <howardmann27@gmail.com>
2016-11-12 17:44:47 +1100

    first commit

---
+++ 2-running-files/app.js
@@ -0,0 +1,20 @@
+// argv index position starts at 2. E.g. if terminal command is node app 1 then 0 is node app is 1 and 1 is 2
+
+// First argument will be command string
+var command = process.argv[2];
+
+// Second and third command will be our a & b variables
+var a = Number(process.argv[3]);
+var b = Number(process.argv[4]);
+
+if (command === 'add') {
+  console.log(a + b);
+} else if (command === 'subtract') {
+  console.log(a - b);
+} else if (command === 'multiply') {
+  console.log(a * b);
+} else if (command === 'divide') {
+  console.log(a / b);
+} else {
+  console.log("command not recognised");
+}
---
+++ 3-commonjs-modules/app.js
@@ -0,0 +1,24 @@
+// Import JavaScript file from same directory
+// Stores it as an object in math variable
+var math = require('./math.js');
+
+console.log(math);
+// In terminal run node app will return an object literal from './math.js'
+/*
+{ add: [Function: add],
+  subtract: [Function: subtract],
+  multiply: [Function: multiply] }
+*/
+
+var command = process.argv[2];
+var a = Number(process.argv[3]);
+var b = Number(process.argv[4]);
+
+// Equivalent to accessing the math object literal and selecting the property variable passed as command which returns a function accepting two arguments (a,b) which we pass from the terminal argv
+var value = math[command](a,b);
+
+console.log(value);
+
+// In terminal:
+// ~ node app add 1 2 => 3
+// ~ node app subtract 1 2 => -1
